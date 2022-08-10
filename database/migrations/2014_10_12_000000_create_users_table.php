b4ab4a27ea9752f25db5b1ea96e459c4e78e6045
62811 <fando@fando.id>
2022-08-10 17:40:23 +0700

    ff

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',256);
+            $table->string('email',256)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',256);
             $table->rememberToken();
             $table->timestamps();
         });
